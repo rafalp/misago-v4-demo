2836c523a6240a00f222566159dddcb5cf6f8985
rafalp <kontakt@rpiton.com>
2022-06-13 00:03:35 +0200

    Update demo warning plugin manifest

--- plugins/demowarning/demowarning/__init__.py
+++ plugins/demowarning/demowarning/__init__.py
@@ -3,10 +3,10 @@ from misago.plugins import PluginManifest
 
 __manifest__ = PluginManifest(
     name="Demo Warning",
-    description="Displays block with warning about side being demo only.",
+    description="Displays block with warning about site being a demo.",
     license="BSD-3",
-    icon="fas fa-exclamation",
-    color="3581b8",
+    icon="fas fa-exclamation-triangle",
+    color="e67e22",
     version="0.1.0",
     author="Rafał Pitoń",
     homepage="https://misago-project.org",
